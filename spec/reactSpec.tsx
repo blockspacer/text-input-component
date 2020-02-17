9464a3cf786fe9de39c1bbf136db7d5bf222e5c2
York Yao <me@yorkyao.com>
2020-02-17 20:00:12 +0800

    fix: test failure

--- spec/reactSpec.tsx
+++ spec/reactSpec.tsx
@@ -4,7 +4,7 @@ import { TextInput } from '../packages/react/dist'
 import renderer from 'react-test-renderer'
 
 it('renders without crashing', () => {
-  const app = renderer.create(<TextInput data={undefined} />)
+  const app = renderer.create(<TextInput />)
   const rendered = app.toJSON()
   expect(rendered).toBeTruthy()
   app.unmount()
